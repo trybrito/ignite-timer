d11fa7482d530ecbb7b314f00a9c52aa47f01396
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-01-23 18:41:48 -0300

    styles: history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,48 @@
+import { HistoryContainer, HistoryTableContainer, Status } from './styles';
+
 export function History() {
-  return <h1>History</h1>;
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryTableContainer>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor={'green'}>Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor={'yellow'}>Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor={'red'}>Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryTableContainer>
+    </HistoryContainer>
+  );
 }
