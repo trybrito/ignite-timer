42ffaf79f4af3b2e8be5ecc7f5b6fb0176b7503d
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-01-23 14:55:59 -0300

    chore: styles configs & libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
+import { Button } from "./components/Button"
+
 export function App() {
-  return <h1>Hello, World</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -mos-osx-font-smoothing: grayscale;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']}
+  }
+
+  body {
+    background-color: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme['gray-300']};
+  }
+
+  body, input, textarea, button {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  input::placeholder {
+    color: inherit;
+  }
+
+  button {
+    border: 0;
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
