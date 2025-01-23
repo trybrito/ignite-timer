a08ad27c7d665b033734d7f20eca93429a629fcd
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-01-23 20:45:05 -0300

    feat: form validation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,18 +8,52 @@ import {
   TaskInput,
   MinutesAmountInput,
 } from './styles';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const newCycleFormSchema = z.object({
+  task: z.string().min(1, 'Informe uma tarefa válida'),
+  minutesAmount: z
+    .number()
+    .min(5, { message: 'O ciclo precisa ter pelo menos 5 minutos' })
+    .max(60, { message: 'O ciclo não pode ter mais que 60 minutos' }),
+});
+
+type newCycleFormData = z.infer<typeof newCycleFormSchema>;
 
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<newCycleFormData>({
+    resolver: zodResolver(newCycleFormSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 5,
+    },
+  });
+
+  function handleCreateNewCycle(data: newCycleFormData) {
+    console.log(data);
+    reset();
+  }
+
+  const task = watch('task');
+  const isSubmitButtonDisabled = !task;
+
   return (
     <HomeContainer>
       <div>
-        <FormContainer action="#" id="taskForm">
+        <FormContainer
+          action="#"
+          id="taskForm"
+          onSubmit={handleSubmit(handleCreateNewCycle)}
+        >
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             type="text"
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -34,6 +68,7 @@ export function Home() {
             min={5}
             max={60}
             step={5}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -47,7 +82,11 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit" form="taskForm">
+        <StartCountdownButton
+          type="submit"
+          form="taskForm"
+          disabled={isSubmitButtonDisabled}
+        >
           <Play width={20} height={24} />
           Começar
         </StartCountdownButton>
